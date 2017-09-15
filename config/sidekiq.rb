54b9a00965abbb0e8567407df7cd36109f420368
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-09-15 12:07:25 +0100

    configure client

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis-test.ijpwsg.ng.0001.euw2.cache.amazonaws.com:6379' }
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis-test.ijpwsg.ng.0001.euw2.cache.amazonaws.com:6379' }
+end
